03f09edb075d50d69f8fec2cc9f1ec06307506ac
APAC\rahset01 <rahul.seth@protiviti.co.in>
2025-05-23 01:43:34 +0530

    Angular template driven forms

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: false,
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+  formSubmitted = false;
+
+  formData: any = {
+    name: '',
+    email: '',
+    age: '',
+    dob: '',
+    message: '',
+    country: '',
+    gender: '',
+    terms: false,
+    file: null
+  };
+
+  onSubmit(form: NgForm) {
+    this.formSubmitted = true;
+    if (form?.valid) {
+      localStorage.setItem('formData', JSON.stringify(this.formData));
+      alert('Form submitted successfully!');
+      form?.resetForm();
+      this.formSubmitted = false;
+    }
+  }
+
+  onReset(form: NgForm) {
+    form?.resetForm();
+    this.formSubmitted = false;
+  }
+
+  onFileChange(event: any) {
+    const file = event?.target?.files[0];
+    this.formData.file = file ? file?.name : null;
+  }
+}
+
