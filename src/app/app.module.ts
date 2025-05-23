fca98f03697e09ec5f76cc6f626dd8f376726007
APAC\rahset01 <rahul.seth@protiviti.co.in>
2025-05-23 16:29:39 +0530

    fixed conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
 
@@ -17,26 +15,6 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule
-=======
-=======
-import { FormComponent } from './form/form.component';
-import { FormsModule } from '@angular/forms';
->>>>>>> 34e1914 (Angular template driven forms)
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent
-  ],
-  imports: [
-    BrowserModule,
-<<<<<<< HEAD
-    AppRoutingModule
->>>>>>> 603009e (Rebase)
-=======
-    AppRoutingModule,
-    FormsModule
->>>>>>> 34e1914 (Angular template driven forms)
   ],
   providers: [],
   bootstrap: [AppComponent]
