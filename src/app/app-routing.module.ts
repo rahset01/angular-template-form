6f004335c15c428c2963bf388382d87ab5ba3eb5
APAC\rahset01 <rahul.seth@protiviti.co.in>
2025-05-23 16:09:31 +0530

    Prepare for GitHub Pages deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  { path: 'form', component: FormComponent },
+  // add other routes here
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
